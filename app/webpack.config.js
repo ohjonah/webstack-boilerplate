e7f71cde56bdbd0b2679958322019f8ae81d43cc
ohjonah <ohjonah@gmail.com>
2020-12-09 08:47:08 -0800

    Add logging to figure out build bug

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,52 +4,55 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: { importLoaders: 1 },
-                    },
-                    'postcss-loader',
-                ],
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            },
+module.exports = (env) => {
+    console.log('process:', process.env);
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: { importLoaders: 1 },
+                        },
+                        'postcss-loader',
+                    ],
+                },
+                {
+                    test: /\.svg$/,
+                    use: ['@svgr/webpack'],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx'],
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new CleanWebpackPlugin(),
+            new Dotenv(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html'),
+            }),
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin(),
-        new Dotenv(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-        }),
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        hot: true,
-        historyApiFallback: true,
-        open: true,
-    },
-};
+        devServer: {
+            contentBase: path.resolve(__dirname, 'dist'),
+            hot: true,
+            historyApiFallback: true,
+            open: true,
+        },
+    };
+}
