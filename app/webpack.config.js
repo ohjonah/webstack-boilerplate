0f32f4d861e7d9def28221396112c1e5dbe82c44
ohjonah <ohjonah@gmail.com>
2020-12-10 14:55:44 -0800

    amends output path

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = () => {
         entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'dist'),
-            publicPath: path.resolve(__dirname, 'dist'),
+            publicPath: path.resolve(__dirname, '/'),
             filename: 'bundle.js',
         },
         module: {
