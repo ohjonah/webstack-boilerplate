2062de40418f76f9fe3799a19b164862458c3a44
ohjonah <ohjonah@gmail.com>
2020-12-11 23:49:17 -0800

    Adds define plugin

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -42,6 +42,20 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new CleanWebpackPlugin(),
         new Dotenv(),
+        new webpack.DefinePlugin({
+            FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+            FIREBASE_AUTH_DOMAIN: JSON.stringify(
+                process.env.FIREBASE_AUTH_DOMAIN
+            ),
+            FIREBASE_DB_URL: JSON.stringify(process.env.FIREBASE_DB_URL),
+            FIREBASE_PROJECT_ID: JSON.stringify(
+                process.env.FIREBASE_PROJECT_ID
+            ),
+            FIREBASE_STORAGE_BUCKET: JSON.stringify(
+                process.env.FIREBASE_STORAGE_BUCKET
+            ),
+            FIREBASE_SENDER_ID: JSON.stringify(process.env.FIREBASE_SENDER_ID),
+        }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
         }),
